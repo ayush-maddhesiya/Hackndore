6022c334a6011e403ab755ea28cf295f57023fd5
tanay-nagde <tanaynagde@gmail.com>
2024-07-27 01:22:17 +0530

    login and signup page created

---
+++ frontend/hackndore/src/pages/Login.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import indoreImg from '../assets/indore.jpeg'
+
+
+function Login() {
+  return (
+    <> 
+    <section className='py-10 w-11/12 mx-auto '>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+          <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Sign in</h2>
+            
+            <form action="#" method="POST" className="mt-8">
+              <div className="space-y-5">
+                <div>
+                  <label htmlFor="" className="text-base font-medium text-gray-900">
+                    {' '}
+                    Email address{' '}
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      type="email"
+                      placeholder="Email"
+                    ></input>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="" className="text-base font-medium text-gray-900">
+                      {' '}
+                      Password{' '}
+                    </label>
+                    <a
+                      href="#"
+                      title=""
+                      className="text-sm font-semibold text-black hover:underline"
+                    >
+                      {' '}
+                      Forgot password?{' '}
+                    </a>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      type="password"
+                      placeholder="Password"
+                    ></input>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    type="button"
+                    className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                  >
+                    Get started 
+                  </button>
+                </div>
+              </div>
+            </form>
+            <div className="mt-3 space-y-3">
+            <p className="mt-2 text-sm text-gray-600">
+              Don&apos;t have an account?{' '}
+              <a
+                href="#"
+                title=""
+                className="font-semibold text-black transition-all duration-200 hover:underline"
+              >
+                Create a free account
+              </a>
+            </p>    
+            </div>
+          </div>
+        </div>
+        <div className="h-full w-full">
+          <img
+            className="mx-auto h-full w-full rounded-md object-cover"
+            src= {indoreImg}
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default Login
