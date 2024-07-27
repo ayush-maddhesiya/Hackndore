3a05c4803cd32d8e12cb35d62ebd28b8f56f0a86
TANISH <tanishjain184@gmail.com>
2024-07-27 13:44:47 +0530

    data export

--- data_processing/csv_generator.py
+++ data_processing/csv_generator.py
@@ -2,15 +2,35 @@ import pymongo
 import pymongo.mongo_client
 import pandas as pd
 
-def data_fetch():
+def data_export():
     if __name__ == "__main__":
         client=pymongo.MongoClient( "mongodb+srv://ayush:ayush123@cluster0.9h4qgcg.mongodb.net/")
         print(client)
         db=client["Revenue_Collection"]
         garb_taxes=db["garbagetaxes"]
         water_tax=db["watertaxes"]
+        property_tax=db["propertytaxes"]
+
         garb_tax_list=list(garb_taxes.find())
         water_tax_list=list(water_tax.find())
-        print(garb_tax_list,water_tax_list)
+        prop_tax_list=list(property_tax.find())
 
-data_fetch()        
+        garb_df=pd.DataFrame(garb_tax_list)
+        water_df=pd.DataFrame(water_tax_list)
+        prop_df=pd.DataFrame(prop_tax_list)
+        
+        if '_id' in garb_df:
+            del garb_df['_id']
+        if '_id' in water_df:
+            del water_df['_id']
+        if '_id' in prop_df:
+            del prop_df['_id']
+
+        garb_df.to_csv("garbage_tax.csv")
+        water_df.to_csv("water_tax.csv")
+        prop_df.to_csv("property_tax.csv")
+       
+        
+
+data_export()   
+   
