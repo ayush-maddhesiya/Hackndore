876fb276e7b5d122596339d20296f644973b8f93
TANISH <tanishjain184@gmail.com>
2024-07-27 19:26:08 +0530

    bill generation completed

---
+++ bill_generator/cust_details.py
@@ -0,0 +1,29 @@
+import pymongo
+import pymongo.mongo_client
+import pandas as pd
+from bill_gen import BillGenerators
+import pandas as pd
+
+class Get_data:
+    @staticmethod
+    def selectdata(bill_no):
+       
+            client=pymongo.MongoClient( "mongodb+srv://ayush:ayush123@cluster0.9h4qgcg.mongodb.net/")
+            print(client)
+            db=client["Revenue_Collection"]
+            collection_user=db['users']
+            
+            collection=db["payments"]
+            a=collection.find_one({'billNumber':bill_no})
+            user=collection_user.find_one({'_id':a['user']})
+            if a['bill']=="propertytax":
+                  BillGenerators.property_bill(user['fullName'],a['_id'],a['billNumber'])
+            elif a['bill']=="watertax":
+                  BillGenerators.water_bill(user['fullName'],a['_id'],a['billNumber'])
+            elif a['bill']=="garbagetax":
+                  BillGenerators.garbage_bill(user['fullName'],a['_id'],a['billNumber'])
+      
+      
+
+            
+
